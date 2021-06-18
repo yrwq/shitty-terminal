ea046b29539be79a6d9bf7d0d44b4438c6e3bd97
Inhof Dávid <yrwqid@gmail.com>
2021-06-18 13:18:14 +0200

    update font

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Terminus:pixelsize=12:antialias=true:autohint=true";
-static char *font2[] = { "Twitter Color Emoji:pixelsize=12:antialias=true:autohint=true" };
+static char *font2[] = { "TerminessTTF Nerd Font:pixelsize=12:antialias=true:autohint=true" };
 static int borderpx = 30;
 
 /*
